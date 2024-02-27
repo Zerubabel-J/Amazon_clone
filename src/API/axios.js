2c09c0e339a87ae91afeb6df75c5ad93752bca5e
Zerubabel <zerucodes@gmail.com>
2024-02-27 12:30:13 +0300

    axios baseUrl updated

--- src/API/axios.js
+++ src/API/axios.js
@@ -2,10 +2,10 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   // local instance of firebase functions
-  baseURL: "http://127.0.0.1:5001/clone-724be/us-central1/api",
+  // baseURL: "http://127.0.0.1:5001/clone-724be/us-central1/api",
 
   //deployed version of amazon server on render.com
-  // baseURL: "https://amazon-api-deploy-pm1z.onrender.com",
+  baseURL: "https://amazon-api-deploy-2f7q.onrender.com/",
 });
 
 export { axiosInstance };
